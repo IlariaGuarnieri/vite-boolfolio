5a3ebff2a6eef44aac4ad88ac148983f4293e55d
IlariaGuarnieri <ilaria.guarnieri@gmail.com>
2024-05-31 18:26:50 +0200

    [add] fine stampain pagina, metà milestone 5

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,25 @@
 <script>
 import axios from 'axios'
+import {store} from './data/store'
 
   export default {
     data(){
       return{
-        'projects' : []
+        'projects' : [],
+        loading : true,
       }
     },
       methods:{
         getApi(){
           console.log('get Api');
-          axios.get('http://127.0.0.1:8000/api/projects')
+          axios.get(store.apiUrl)
           .then(result=>{
+            this.loading = false;
             this.projects = result.data
             console.log(this.projects);
           })
           .catch(error=>{
+            this.loading = false;
             console.log(error);
 
           })
@@ -33,10 +37,13 @@ import axios from 'axios'
     <div class="container">
       <div>
       <h1>Boolfolio Home</h1>
-    <h5>i miei progetti:</h5>
+    <div v-if="!loading">
+      <h5>i miei progetti:</h5>
     <ul>
       <li v-for="project in projects " :key="project.id" >{{project.id}}-{{project.title}}</li>
     </ul>
+    </div>
+    <p v-else> Loading...</p>
     </div>
 
     </div>
